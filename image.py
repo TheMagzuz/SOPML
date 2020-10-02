4c1461e4cd1d48d090f162fee67e3b56539c2238
Markus Dam <markus.dam123@gmail.com>
2020-10-02 20:11:50 +0200

    Add image class

---
+++ image.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+class Image:
+    def __init__(self, data, width: int, height: int):
+        self.data = data
+        self.matrixData = np.empty(0)
+        self.width = width
+        self.height = height
+
+    def Data2D(self, forceRecalculate=False):
+        if not forceRecalculate and self.matrixData.size != 0:
+            return self.matrixData
+
+        self.matrixData = np.empty((self.width, self.height))
+
+        for row in range(self.height):
+            for col in range(self.width):
+                self.matrixData[row, col] = self.data[row * self.width + col]
+
+        return self.matrixData
